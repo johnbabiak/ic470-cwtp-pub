f6facb91eac0ec09e7f5722592f0a262d28f1ead
John Babiak <m230270@usna.edu>
2023-04-10 08:04:54 -0400

    testing making link bigger

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ apk add php
 apk add python3
 apk add php-session
 apk add php-sqlite3
+apk add figlet
 echo "cwtp.db" >> ~/ic470-cwtp-pub/.git/info/exclude
 cat ~/ic470-cwtp-pub/create_table.txt | sqlite3 ~/ic470-cwtp-pub/cwtp.db 
 chmod +x run
